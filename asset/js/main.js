4ad5c97ebc4afb3c4e2dc6f9fa082120d43f9cc2
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-04-04 08:45:13 +0200

    v-for added

---
+++ asset/js/main.js
@@ -0,0 +1,49 @@
+//Vue.js
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            products: [
+                {
+                    id: 1,
+                    name: 'nike',
+                    price: 120,
+                    media: 'https://source.unsplash.com/random/400x200/?shoes'
+                },
+                {
+                    id: 2,
+                    name: 'adidas',
+                    price: 110,
+                    media: 'https://source.unsplash.com/random/400x200/?shoes'
+                },
+                {
+                    id: 3,
+                    name: 'vans',
+                    price: 90,
+                    media: 'https://source.unsplash.com/random/400x200/?shoes'
+                },
+                {
+                    id: 4,
+                    name: 'jordan',
+                    price: 160,
+                    media: 'https://source.unsplash.com/random/400x200/?shoes'
+                },
+                {
+                    id: 5,
+                    name: 'puma',
+                    price: 60,
+                    media: 'https://source.unsplash.com/random/400x200/?shoes'
+                },
+                {
+                    id: 6,
+                    name: 'superga',
+                    price: 70,
+                    media: 'https://source.unsplash.com/random/400x200/?shoes'
+                },
+            ]
+        },
+        methods: {
+
+        }
+    }
+)
