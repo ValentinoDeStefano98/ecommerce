aa1e66855ad415ed5ef9f5a47431244727fac82b
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-04-05 18:14:39 +0200

    Finish

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,46 +6,71 @@ var app = new Vue(
             products: [
                 {
                     id: 1,
-                    name: 'nike',
+                    name: 'Nike',
                     price: 120,
                     media: 'https://source.unsplash.com/random/400x200/?shoes'
                 },
                 {
                     id: 2,
-                    name: 'adidas',
+                    name: 'Adidas',
                     price: 110,
                     media: 'https://source.unsplash.com/random/400x200/?shoes'
                 },
                 {
                     id: 3,
-                    name: 'vans',
+                    name: 'Vans',
                     price: 90,
                     media: 'https://source.unsplash.com/random/400x200/?shoes'
                 },
                 {
                     id: 4,
-                    name: 'jordan',
+                    name: 'Jordan',
                     price: 160,
                     media: 'https://source.unsplash.com/random/400x200/?shoes'
                 },
                 {
                     id: 5,
-                    name: 'puma',
+                    name: 'Puma',
                     price: 60,
                     media: 'https://source.unsplash.com/random/400x200/?shoes'
                 },
                 {
                     id: 6,
-                    name: 'superga',
+                    name: 'Superga',
                     price: 70,
                     media: 'https://source.unsplash.com/random/400x200/?shoes'
                 },
             ],
             carrello: [],
         },
+        created(){
+
+        },
         methods: {
+            //Funzione per aggiungere l'elemento al carrello
             addToCart: function(element){
                 this.carrello.push(element);
+            },
+            //Funzione per rimuovere l'elemento dal carrello
+            removeToCart: function(id){
+                this.carrello = this.carrello.filter(
+                    (element) => {
+                        if(element.id != id){
+                        return true;
+                    }
+                        return false;
+                    }
+                );
+            },
+            //Funzione per calcolare il totale del carrello
+            sommaPrezzo: function(){
+                let total = 0;
+                this.carrello.forEach(
+                    (element) => {
+                        total += element.price;
+                    }
+                );
+                return total;
             }
         }
     }
